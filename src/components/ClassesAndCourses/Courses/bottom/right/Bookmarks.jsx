84e6f8085cf58da8a03c71a6cd51034ce678e046
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-05-03 23:44:41 +0530

    edu-devora v1 up: updated course section: added bookmark functionality -> add, show, and remove bookmarks

--- src/components/ClassesAndCourses/Courses/bottom/right/Bookmarks.jsx
+++ src/components/ClassesAndCourses/Courses/bottom/right/Bookmarks.jsx
@@ -12,9 +12,12 @@ const Bookmarks = ({bmData, CData}) => {
     })
 
 
-const removeBookmark = (id)=> {
-  console.log(id)
-  return bookmarkData.splice(id, 1);
+const removeBookmark = (idx)=> {
+  setbookmarkData(() => {
+    return bookmarkData.filter((values)=> {
+    return values.cId !== idx
+    })
+  })
 }
 
 // console.log(cName);
@@ -22,13 +25,13 @@ const removeBookmark = (id)=> {
 
 
   return (
-    <div className='bg-black/20 text-white rounded-[10px] borders backdrop-blur-md shadow-m h-100 hover:scale-105 transition-all'>
+    <div className='bg-black/20 text-white rounded-[10px] borders backdrop-blur-md shadow-m h-100 hover:scale-105 transition-all overflow-y-auto bmkDiv'>
       <div className='w-full flex justify-center h-15 items-center border-b-2 border-b-[#4d2f976d]'>
         <h1 className='bg-[#f69d11] px-5 py-2 rounded-[10px] text-[#0e162a] font-bold'>Bookmarks</h1>
       </div>
 
       <div className="flex items-center justify-between px-2 py-3">
-        <ul className="w-full flex flex-col gap-3">
+        <ul className="w-full flex flex-col gap-3 ">
         {filteredArr.map(({id, courseName})=> {
           return <li key={id} className="flex justify-between">
             <Link className="bmkCname courseCards">{courseName}</Link>

90c8460e1056e8dc134b91cd9a0da745ee3fd77d
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-05-09 18:58:51 +0530

    edu-devora v1 up: adding devora ai chatbot

---
+++ src/components/DevoraChatBox.jsx
@@ -0,0 +1,10 @@
+
+const DevoraChatBox = () => {
+  return (
+    <div className='w-100 h-120 bg-black/30 text-white rounded-[10px] borders backdrop-blur-md shadow-md devoraChatBox '>
+        hello
+    </div>
+  )
+}
+
+export default DevoraChatBox
--- src/components/navbar/devora.jsx
+++ src/components/navbar/devora.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'lucide-react';
 import devoralogo from "../../assets/devora.png"
 import { useState } from 'react';
+import DevoraChatBox from '../DevoraChatBox';
 
 export default function Devora() {
+  const [isClicked, setisClicked] = useState(false)
+  console.log(isClicked)
   return (
-    <div >
-      <button className='devora-ai flex items-center'>
+    <div className='flex flex-col items-end'>
+      <div>
+        {isClicked && <DevoraChatBox />}
+      </div>
+      <button className='devora-ai flex items-center' onClick={()=> setisClicked(!isClicked)}>
             <p className='devoraText'></p>
             <img src={devoralogo} alt="devora" className='w-30 cursor-pointer'/>
         </button>
